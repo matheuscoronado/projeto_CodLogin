874af9a04d0ab28187cf40d474bc572a1ab57edd
Matheus Coronado <matheuscoronado48@gmail.com>
2025-04-15 19:39:00 -0300

    Criado função 'acessar' para o Email e Senha

---
+++ main.js
@@ -0,0 +1,5 @@
+// Implementa acesso para a tela de Login
+function acessar(){
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+}
