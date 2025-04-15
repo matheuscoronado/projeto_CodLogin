d2fe3499a366d65f0db2b3fb0c45b6537dccf042
Matheus Coronado <matheuscoronado48@gmail.com>
2025-04-15 19:42:17 -0300

    Criado um IF para testar se ambos os campos de Email e Senha foram preenchidos

--- main.js
+++ main.js
@@ -2,4 +2,10 @@
 function acessar(){
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
+
+    // Implementa estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos!");
+        return;
+    }
 }
